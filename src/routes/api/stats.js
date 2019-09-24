2658b82174216482087a90451f2f9ce6d70a932c
Domingo Ramirez <djramirez@uc.cl>
2019-09-24 08:33:42 -0300

    Comment parse

--- src/routes/api/stats.js
+++ src/routes/api/stats.js
@@ -2,16 +2,16 @@ const KoaRouter = require('koa-router');
 
 const router = new KoaRouter();
 
-router.post('api.calculate', '/', async (ctx) => {})
+
 
 router.get('api.get_paciente', '/get_paciente', async (ctx) => {
-  let query = JSON.parse(ctx.request.query);
+  // let query = JSON.parse(ctx.request.query);
   let responseData =  {nombre: 'Arturo Vidal',fecha_nacimiento: '12 septiembre 2019',nombre_padre: 'Pablo Vidal',nombre_madre: 'Ana Vidal' };
   ctx.body = JSON.stringify(responseData);
 });
 
 router.get('api.get_consulta', '/get_consulta', async (ctx) => {
-  let query = JSON.parse(ctx.request.query);
+  // let query = JSON.parse(ctx.request.query);
   let responseData =  {peso: 3.45, diametro_cranel: 33, altura: 34, comentario:'bebe muy bonito, sera un gran jugador'};
   ctx.body = JSON.stringify(responseData);
 });
